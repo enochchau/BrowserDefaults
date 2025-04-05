6a19c8a2813daa470afe57b39a7cd73cfd9d4a5b
ec965 <enoch965@gmail.com>
2025-04-05 15:04:44 -0700

    Initial Commit

---
+++ BrowserDefaults/BrowserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserDefaultsApp.swift
+//  BrowserDefaults
+//
+//  Created by Enoch Chau on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BrowserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrowserDefaultsTests/BrowserDefaultsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserDefaultsTests.swift
+//  BrowserDefaultsTests
+//
+//  Created by Enoch Chau on 4/5/25.
+//
+
+import Testing
+@testable import BrowserDefaults
+
+struct BrowserDefaultsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
